9a07ba6d8afba86138b7c287c64c1b47656780db
ranspektoraidock <ranspektor@gmail.com>
2020-08-13 10:58:26 +0300

    Updates to deploy to heroku

--- libs/models/url.consts.ts
+++ libs/models/url.consts.ts
@@ -1,7 +1,7 @@
 
 export const ACCU_WEATHER_KEY : Key = { key : 'v9AZ0ip3iurZ6w9beq37qgkqV4OvtQwx'};
-export const AUTO_COMPLETE_SEARCH_URL = 'http://dataservice.accuweather.com/locations/v1/cities/autocomplete';
-export const FIVE_DAYS_FORECAST_BY_LOCATION_KEY_URL = 'http://dataservice.accuweather.com/forecasts/v1/daily/5day';
+export const AUTO_COMPLETE_SEARCH_URL = 'https://cors-anywhere.herokuapp.com/http://dataservice.accuweather.com/locations/v1/cities/autocomplete';
+export const FIVE_DAYS_FORECAST_BY_LOCATION_KEY_URL = 'https://cors-anywhere.herokuapp.com/http://dataservice.accuweather.com/forecasts/v1/daily/5day';
 
 export interface Key {
   key: string
