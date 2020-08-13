b319ca3f1f1932cff29f9605bb354db446fba71b
ranspektoraidock <ranspektor@gmail.com>
2020-08-13 11:13:09 +0300

    New key

--- libs/models/url.consts.ts
+++ libs/models/url.consts.ts
@@ -1,5 +1,5 @@
 
-export const ACCU_WEATHER_KEY : Key = { key : 'v9AZ0ip3iurZ6w9beq37qgkqV4OvtQwx'};
+export const ACCU_WEATHER_KEY : Key = { key : 'xlafDHnO5F97sf5D44OE9A8eEvgeEARe'};
 export const AUTO_COMPLETE_SEARCH_URL = 'https://cors-anywhere.herokuapp.com/http://dataservice.accuweather.com/locations/v1/cities/autocomplete';
 export const FIVE_DAYS_FORECAST_BY_LOCATION_KEY_URL = 'https://cors-anywhere.herokuapp.com/http://dataservice.accuweather.com/forecasts/v1/daily/5day';
 
